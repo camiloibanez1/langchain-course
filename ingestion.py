769cf6b4aa8873c8a56d39c98bbcf5a4a0092ce5
CamiloI <camiloi@quinnox.com>
2026-07-09 14:36:28 -0500

    init

--- ingestion.py
+++ ingestion.py
@@ -1,9 +1,35 @@
 import os
-from dotenv import load_dotenv
 
+from dotenv import load_dotenv
+from langchain_openai import OpenAIEmbeddings
+from langchain_unstructured import UnstructuredLoader
+from langchain_pinecone import PineconeVectorStore
+from langchain_text_splitters import CharacterTextSplitter
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
 
 load_dotenv()
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     print("Ingesting...")
+    loader = UnstructuredLoader(
+        file_path="mediumblog1.txt", chunking_strategy="basic", max_characters=1000000
+    )
+    document = loader.load()
 
+    print("Splitting...")
+    text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+    texts = text_splitter.split_documents(document)
+    print(f"Number of chunks: {len(texts)}")
+
+    embeddings = GoogleGenerativeAIEmbeddings(
+        model="gemini-embedding-2-preview", output_dimensionality=1536
+    )
+
+    print("ingesting...")
+    PineconeVectorStore.from_documents(
+        documents=texts,
+        embedding=embeddings,
+        index_name=os.environ.get("INDEX_NAME"),
+    )
+
+    print("Ingestion complete.")
